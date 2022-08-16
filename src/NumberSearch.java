296eb3133316c0676f7479062a4a8338eb2eb790
gnidinger <gcnb87@gmail.com>
2022-08-16 18:54:01 +0900

    NumberSearch

--- src/NumberSearch.java
+++ src/NumberSearch.java
@@ -1,12 +1,13 @@
 // 문자열을 입력받아 문자열에서 숫자를 모두 찾아 더한 뒤에 해당 값을
 // (숫자와 공백을 제외한 나머지) 문자열의 길이로 나눈 값을 정수로 반올림하여 리턴
 
-public class numberSearch {
+public class NumberSearch {
 
     public static void main(String[] args) {
         String str = "Hello6 9World 2, Nic8e D7ay!";
 
         System.out.println(search(str));
+        System.out.println((char)(str.charAt(1) + 3));
     }
 
     public static int search(String str) {
